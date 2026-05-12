d8b0b110e31377e66c1280fb64ee29a2fa4a10b9
Artem Kargopolov <kur4g1n@gmail.com>
2026-05-12 12:41:21 +0300

    Enabled system clipboard for nvim in ssh

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,3 +2,17 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.g.python3_host_prog = vim.fn.expand("~/.local/bin/pynvim-python")
+
+if os.getenv("SSH_TTY") then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
