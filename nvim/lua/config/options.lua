9aae92accfb9ef47ff32091ea97f4c8bde780f3a
Artem Kargopolov <kur4g1n@gmail.com>
2026-05-12 18:45:20 +0300

    Revert nvim buffer changes

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,11 +3,6 @@
 -- Add any additional options here
 vim.g.python3_host_prog = vim.fn.expand("~/.local/bin/pynvim-python")
 
--- Keep every everything except yank away from clobbering system clipboard.
--- Yank event is configured in autocmds.lua
-vim.opt.clipboard = ""
-
--- Support for ssh sessions system clipboard
 if os.getenv("SSH_TTY") then
   vim.g.clipboard = {
     name = "OSC 52",
